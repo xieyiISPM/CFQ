ed9dfd35395ca2c5e7b532473f7aa3a625a2c63d
Yi Xie <yixie@u.boisestate.edu>
2019-01-15 12:07:28 -0700

    Proxy test file added

--- src/main/java/afgh/AFGHKeyGen.java
+++ src/main/java/afgh/AFGHKeyGen.java
@@ -1,4 +1,25 @@
 package afgh;
 
+import java.math.BigInteger;
+import java.net.SecureCacheResponse;
+import java.security.SecureRandom;
+
 public class AFGHKeyGen {
+    private int keySize = 1024;
+    private int bitToByte = 8;
+    public AFGHKeyGen(){
+
+    }
+    public AFGHKeyGen ( int keySize){
+        this.keySize = keySize;
+    }
+
+    public BigInteger genPrivateKey(){
+        BigInteger sk;
+        SecureRandom secureRandom = new SecureRandom();
+        byte bytes[] = new byte[keySize/bitToByte];
+        secureRandom.nextBytes(bytes);
+        sk = new BigInteger(1,bytes);
+        return sk;
+     }
 }
--- src/test/java/ProxyTest.java
+++ src/test/java/ProxyTest.java
@@ -1,2 +1,13 @@
+import afgh.AFGHKeyGen;
+
+import java.math.BigInteger;
+
 public class ProxyTest {
+
+    public static void main(String[]  args){
+        AFGHKeyGen afgh = new AFGHKeyGen(1024);
+        BigInteger sk = afgh.genPrivateKey();
+        System.out.println("private key: " + sk);
+        System.out.println("private key bitlength: " + sk.bitLength());
+    }
 }
